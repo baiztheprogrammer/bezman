6ccac9d12bafee48fa0ccbeb5294d66caf220ce2
baiztheprogrammer <kyloren4206969@gmail.com>
2020-12-03 12:55:19 +0600

    added urls to customer stuff

--- bezman_shop/accounts/urls.py
+++ bezman_shop/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import *
+
 
 urlpatterns = [
-
-
+    path('customers/',customerList,name='customers'),
+    path('customers/<int:customer_id>/',getCustomer,name='customer'),
+    path('customercreate/',createCustomer,name='customercreate'),
 ]
