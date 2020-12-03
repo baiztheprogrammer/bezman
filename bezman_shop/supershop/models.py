15c1f87ec04cd6023120cd7092e1f43b23025bd7
baiztheprogrammer <kyloren4206969@gmail.com>
2020-12-03 12:54:57 +0600

    models in supershop

--- bezman_shop/supershop/models.py
+++ bezman_shop/supershop/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from accounts.models import Customer
+
 
 # Create your models here.
 
@@ -46,6 +48,7 @@ class Order(models.Model):
     product = models.ForeignKey(Product,on_delete=models.SET_NULL,null=True)
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20,choices=statuses,default='Not Delivered')
+    customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,null=True)
 
     def __str__(self):
         return self.product.name + ' ' + self.status
