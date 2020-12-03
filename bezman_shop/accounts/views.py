06146e367299ac29be222e3f133b17d5c4df30ae
baiztheprogrammer <kyloren4206969@gmail.com>
2020-12-03 12:55:46 +0600

    new entire customer function

--- bezman_shop/accounts/views.py
+++ bezman_shop/accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
+
+def customerList(request):
+    customers = Customer.objects.all()
+    context = {'customers':customers}
+    return render(request,'supershop/customer-list.html',context)
+
+def getCustomer(request,customer_id):
+    customer = Customer.objects.get(id=customer_id)
+    orders = customer.order_set.all()
+    context = {'customer':customer,'orders':orders}
+    return render(request,'supershop/get-customer.html',context)
+
+def createCustomer(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('products')
+    context = {'form': form}
+    return render(request, 'supershop/customer-create.html', context)
+
+
