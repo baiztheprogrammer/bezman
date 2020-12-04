91e6dddec015855417d5707a20c18e4dd90af4cc
baiztheprogrammer <kyloren4206969@gmail.com>
2020-12-04 13:08:27 +0600

    added jet

--- bezman_shop/bezman_shop/urls.py
+++ bezman_shop/bezman_shop/urls.py
@@ -19,6 +19,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('jet',include('jet.urls','jet')),
+    path('jet.dashboard',include('jet.dashboard.urls','jet-dashboard')),
     path('admin/', admin.site.urls),
     path('shop/',include('supershop.urls')),
     path('accounts/',include('accounts.urls')),
