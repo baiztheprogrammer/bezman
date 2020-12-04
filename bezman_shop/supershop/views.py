57f92fcc15998fe85035b26765702d07c8eebbf0
baiztheprogrammer <kyloren4206969@gmail.com>
2020-12-04 13:09:05 +0600

    added order status to orderlist

--- bezman_shop/supershop/views.py
+++ bezman_shop/supershop/views.py
@@ -9,7 +9,12 @@ def productList(request):
 
 def orderList(request):
     orders = Order.objects.all()
-    context = {'orders':orders}
+    orders_count = orders.count()
+    orders_delivered = Order.objects.filter(status='Delivered').count()
+    orders_in_process = Order.objects.filter(status='In Process').count()
+    orders_not_delivered = Order.objects.filter(status='Not Delivered').count()
+
+    context = {'orders':orders,'orders_count':orders_count,'orders_delivered':orders_delivered,'orders_in_process':orders_in_process,'orders_not_delivered':orders_not_delivered}
     return render(request,'supershop/order-list.html',context)
 
 def orderCreate(request,product_id):
@@ -21,5 +26,16 @@ def orderCreate(request,product_id):
             form.save()
             return redirect('orders')
 
+    context = {'form':form}
+    return render(request,'supershop/order-create.html',context)
+
+def orderUpdate(request,order_id):
+    order = Order.objects.get(id=order_id)
+    form = OrderForm(instance=order)
+    if request.method == 'POST':
+        form = OrderForm(request.POST,instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('orders')
     context = {'form':form}
     return render(request,'supershop/order-create.html',context)
