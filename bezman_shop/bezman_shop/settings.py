562970db689dcebe621e632a5af8c4d2c1c393ea
baiztheprogrammer <kyloren4206969@gmail.com>
2020-12-07 12:37:49 +0600

    added django filter

--- bezman_shop/bezman_shop/settings.py
+++ bezman_shop/bezman_shop/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'supershop',
-    'accounts'
+    'accounts',
+    'django_filters',
+
 ]
 
 MIDDLEWARE = [
