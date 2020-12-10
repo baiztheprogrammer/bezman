241955c46b78343ab5f4a569da81dc2a1a8ec6b7
baiztheprogrammer <kyloren4206969@gmail.com>
2020-12-10 13:18:59 +0600

    added users onetonefield

--- bezman_shop/accounts/models.py
+++ bezman_shop/accounts/models.py
@@ -1,13 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
     full_name = models.CharField(max_length=50)
     address = models.CharField(max_length=30)
     phone = models.IntegerField()
     image = models.ImageField()
-
-
 
     def __str__(self):
         return self.full_name
