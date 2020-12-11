76077235f2d6ec69cd34cb2c9d93decc5c1413a1
baiztheprogrammer <kyloren4206969@gmail.com>
2020-12-11 12:49:17 +0600

    added password reset

--- bezman_shop/accounts/urls.py
+++ bezman_shop/accounts/urls.py
@@ -3,12 +3,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import *
 
 
 urlpatterns = [
+    path('reset-password/',PasswordResetView.as_view(),name='reset-password'),
+    path('reset-password-done/',PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset-password-complete/',PasswordResetCompleteView.as_view(),name='password_reset_complete'),
     path('customers/',customerList,name='customer'),
     path('customers/<int:customer_id>/',getCustomer,name='customer'),
     path('register/',register,name='register'),
     path('login/',auth,name='login'),
-    path('logout/',logout_page,name='logout')
+    path('logout/',logout_page,name='logout'),
 ]
