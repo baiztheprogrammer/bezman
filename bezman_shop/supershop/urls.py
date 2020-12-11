11d9eefb346f36b4c8d3ee105215c8c9eabf7b1a
baiztheprogrammer <kyloren4206969@gmail.com>
2020-12-11 12:49:48 +0600

    added order update and delete

--- bezman_shop/supershop/urls.py
+++ bezman_shop/supershop/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('products/',productList,name='products'),
     path('orders/',orderList,name='orders'),
     path('order-create/<int:product_id>/',orderCreate,name='order-create'),
-    path('order-update/<int:order_id>/', orderUpdate, name='order-update')
+    path('order-update/<int:order_id>/', orderUpdate, name='order-update'),
+    path('order-delete/<int:order_id>/',orderDelete,name='order-delete'),
 ]
